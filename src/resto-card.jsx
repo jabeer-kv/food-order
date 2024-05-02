7bd66c6ecd89155ed679d582ced30e8c9658b0dd
jabeer-kv <jabeerkv1@gmail.com>
2024-05-02 08:14:25 +0530

    added the hotels

--- src/resto-card.jsx
+++ src/resto-card.jsx
@@ -1,25 +1,23 @@
-
-function Restocard(){
-    return(
-        <div >
-            <div className="resto-img">
-                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS0l0cRpZQ5FeyOA6cPHLKmuAUBo8viqGcg3A&s" alt="" />
-            </div>
-            <div className="resto-info">
-                <h3 className="food-name">
-                   Porrota and Beef
-                </h3>
-                <p className="discription">
-                    a crispy porrotta and beef
-                </p>
-            </div>
-            <div className="resto-btn">
-                <button className="order-button">
-                    order now
-                </button>
-            </div>
-
-        </div>
-    )
+function Restocard(props) {
+    console.log(props);
+  const { name, cat, img } = props.restdata;
+  return (
+    <div  className="resto-card">
+      <div>
+        <img
+          className="resto-img"
+          src={img}
+          alt=""
+        />
+      </div>
+      <div className="resto-info">
+        <h3 className="food-name">{name}</h3>
+        <p className="discription">{cat}</p>
+      </div>
+      <div className="resto-btn">
+        <button className="order-button">order now</button>
+      </div>
+    </div>
+  );
 }
-export default Restocard
+export default Restocard;
